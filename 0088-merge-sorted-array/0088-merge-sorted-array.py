95f9b3a74449ba9139509c6f74995b3fe073f346
Hojun Hwang <voicihojun@gmail.com>
2025-01-28 10:36:23 -0500

    Time: 0 ms (100%), Space: 17.7 MB (50.54%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -4,18 +4,15 @@ class Solution:
         Do not return anything, modify nums1 in-place instead.
         """
         mem_j = 0
-        print(m)
+
         for i in range(m+n):
             for j in range(mem_j, n):
                 if nums1[i] >= nums2[j]:
                     nums1.insert(i, nums2[j])
                     mem_j = j+1  
-                    print("if",i, j, mem_j, nums1, nums2)
                     break                
                 else:
-                    print("else", i, j, mem_j, nums1, nums2)
                     break
-            print(i, mem_j, nums1)
         
         for i in range(n):
             nums1.pop()
